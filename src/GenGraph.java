cf8ed1249718aaf6e4215bad233841f66fe7345a
abhidanviji <abhi_danviji@yahoo.co.in>
2017-03-29 19:23:10 -0400

    Version 1.4
    Working Generation Graph Concept

--- src/GenGraph.java
+++ src/GenGraph.java
@@ -1,14 +1,31 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
 public class GenGraph {
+	public static HashMap<String, Integer> map = new HashMap<>();
+	public static ArrayList<String> al = new ArrayList<>();
+	public static ArrayList<String> gen = new ArrayList<>();
+	public static String val="";
+	public static String findNodes(int parent, boolean flag){
+		val="";
+		for (Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue().equals(parent)) {
+                val=val+entry.getKey()+",";
+                if(flag)
+        			gen.add(entry.getKey());
+            }
+           
+        }
+		
+		 return val.substring(0,val.lastIndexOf(","));
+	}
 
 	public static void main(String[] args) {
-		HashMap<String, Integer> map = new HashMap<>();
-		ArrayList<String> al = new ArrayList<>();
-		String val="";
-		String input = "Sam->Pete,Pete->Nancy,Sam->Katie,Mary->Bob,Frank->Mary,Mary->Sam,Bob->John,Pete";
+		
+		
+		String input = "Frank->Mary,Mary->Sam,Mary->Bob,Sam->Katie,Sam->Pete,Bob->John,Bob";
 		String[] tree = input.split(",");
 		for(int i=0;i<tree.length-1;i++){
 			//System.out.println(tree[i]);
@@ -18,17 +35,17 @@ public class GenGraph {
 			}
 			map.put(s[1], al.indexOf(s[0]));
 		}
+		int parent = map.get(al.get(map.get(tree[tree.length-1])));
+		String p = findNodes(parent,false);
 		
+		String spl[] = p.split(",");
+		for(int i=0;i<spl.length;i++){
+			p = findNodes(al.indexOf(spl[i]),true);
+		}
+		Collections.sort(gen);
+		System.out.println(gen.toString().replace("[","").replace("]","").replace(" ", ""));
 		
-		for (Entry<String, Integer> entry : map.entrySet()) {
-			System.out.println("Key - "+entry.getKey()+" Value - "+entry.getValue());
-            if (entry.getValue().equals(map.get(tree[tree.length-1]))) {
-                val=val+entry.getKey()+",";
-            }
-        }
-		System.out.println(al.get(map.get(tree[tree.length-1])));//parent
-		System.out.println(map.get(al.get(map.get(tree[tree.length-1]))));
-		System.out.println(val.substring(0,val.lastIndexOf(",")));
+		//System.out.println(val.substring(0,val.lastIndexOf(",")));
 	}
 
 }
